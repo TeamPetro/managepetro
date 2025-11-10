a1dc017277efd80440a3ee3cc91159c55c33aa30
sjabri <samijab95@gmail.com>
2025-11-10 07:03:18 -0800

    updating CORS on devmain for deployment testing

--- backend/config.py
+++ backend/config.py
@@ -146,6 +146,7 @@ class Config:
                 "http://127.0.0.1:3000",
                 "http://127.0.0.1:3001",
                 "http://localhost:5173",  # Vite dev server
+                "https://managepetro.vercel.app", 
                 "https://manage-petro-frontend.vercel.app",  # Production frontend
             ]
         
